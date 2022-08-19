d278c8576917d418f4ae21478a881364f67de0ec
courtlandpinnock <courtlandpinnock@gmail.com>
2022-08-19 14:26:17 +0100

    Calculator defaulting back to 0, instead of being blank when all numbers deleted now works

--- script.js
+++ script.js
@@ -174,7 +174,12 @@ function populate() {
         }
     })
     numDel.addEventListener("click", (e) => {
-        display.textContent = display.textContent.slice(0, -1);
+        if (display.textContent != "0") {
+            display.textContent = display.textContent.slice(0, -1);
+        }
+        if (display.textContent === "") {
+            display.textContent = "0";
+        }
         
     })
     numMinus.addEventListener("click", (e) => {
